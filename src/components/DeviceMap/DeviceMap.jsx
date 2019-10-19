e8f44c5a647c7ae648f3c9a7d874b904a89980c9
Elston Aw <elston.ayx@u.nus.edu>
2019-10-19 15:44:48 -0400

    Add landslide data with polygons (#5)

--- src/components/DeviceMap/DeviceMap.jsx
+++ src/components/DeviceMap/DeviceMap.jsx
@@ -1,10 +1,25 @@
-import React from "react";
-import { Map, TileLayer, LayersControl } from "react-leaflet";
+import React, { useState, useEffect } from "react";
+import request from "request-promise-native";
+import { Map, TileLayer, LayersControl, GeoJSON } from "react-leaflet";
 import PropTypes from "prop-types";
 
-const { BaseLayer, Overlay } = LayersControl;
+const { BaseLayer } = LayersControl;
 
 export const DeviceMap = props => {
+  const [geoJSONData, setGeoJSONData] = useState();
+
+  useEffect(() => {
+    request({
+      method: "GET",
+      uri:
+        "https://pmmpublisher.pps.eosdis.nasa.gov/products/s3/Global/global_landslide_nowcast/2019/288/global_landslide_nowcast_20191015.geojson"
+    }).then(data => {
+      const parsedData = JSON.parse(data);
+      setGeoJSONData(parsedData);
+    }, console.log);
+
+    return () => {};
+  });
   return (
     <div style={{ width: "inherit", height: "inherit" }}>
       <Map center={props.position} zoom={10}>
@@ -19,6 +34,7 @@ export const DeviceMap = props => {
             <TileLayer url="https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.png" />
           </BaseLayer>
         </LayersControl>
+        {geoJSONData && <GeoJSON data={geoJSONData} color="red" />}
       </Map>
     </div>
   );
