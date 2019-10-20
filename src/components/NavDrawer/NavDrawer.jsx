a6a37f3cf41d67bac19f0e91ee53ee9f50f4bef4
Elston Aw <elston.ayx@u.nus.edu>
2019-10-20 08:28:59 -0400

    Remove countries in nav bar (#15)

--- src/components/NavDrawer/NavDrawer.jsx
+++ src/components/NavDrawer/NavDrawer.jsx
@@ -5,15 +5,11 @@ import List from "@material-ui/core/List";
 import Divider from "@material-ui/core/Divider";
 import ListItem from "@material-ui/core/ListItem";
 import ListItemText from "@material-ui/core/ListItemText";
-import GlobalContext from "../../GlobalContext.jsx";
 import request from "request-promise-native";
-import PropTypes from 'prop-types';
 
 import { SimpleModal } from "../Modal/Modal.jsx";
 
-
 const drawerWidth = 300;
-
 
 const useStyles = makeStyles(theme => ({
   drawer: {
@@ -25,50 +21,34 @@ const useStyles = makeStyles(theme => ({
   },
   root: {
     background: props =>
-      props.color === 'High'
-        ? 'linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)'
-        : 'linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)',
+      props.color === "High"
+        ? "linear-gradient(45deg, #FE6B8B 30%, #FF8E53 90%)"
+        : "linear-gradient(45deg, #2196F3 30%, #21CBF3 90%)",
     border: 0,
     borderRadius: 3,
     boxShadow: props =>
-      props.color === 'High'
-        ? '0 3px 5px 2px rgba(255, 105, 135, .3)'
-        : '0 3px 5px 2px rgba(33, 203, 243, .3)',
-    color: 'white',
+      props.color === "High"
+        ? "0 3px 5px 2px rgba(255, 105, 135, .3)"
+        : "0 3px 5px 2px rgba(33, 203, 243, .3)",
+    color: "white",
     height: 48,
-    padding: '0 30px',
-    margin: 8,
+    padding: "0 30px",
+    margin: 8
   },
 
   paper: {
-    position: 'absolute',
+    position: "absolute",
     width: 400,
     backgroundColor: theme.palette.background.paper,
-    border: '2px solid #000',
+    border: "2px solid #000",
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
+    padding: theme.spacing(2, 4, 3)
   }
-
 }));
-
-
-const InputButtons = [
-  {
-    title: "Singapore",
-    position: [1.2905, 103.852]
-  },
-  {
-    title: "Bangka",
-    position: [-2.4706, 106.2264]
-  }
-];
-
 
 export const NavDrawer = () => {
   const classes = useStyles();
-  const globalState = React.useContext(GlobalContext);
   const [districtJSONData, setDistrictJSONData] = useState([]);
-
 
   useEffect(() => {
     request({
@@ -79,10 +59,10 @@ export const NavDrawer = () => {
       const parsedData = JSON.parse(data);
       setDistrictJSONData(parsedData);
     }, console.log);
-    
+
     return () => {};
   }, []);
-        
+
   return (
     <Drawer
       className={classes.drawer}
@@ -92,39 +72,29 @@ export const NavDrawer = () => {
         paper: classes.drawerPaper
       }}
     >
-      <Divider />
-      <List>
-        {InputButtons.map((item, index) => {
-          return (
-            <ListItem
-              button
-              key={index}
-              onClick={() => {
-                globalState.setState({ position: item.position });
-              }}
-            >
-              <ListItemText primary={item.title} />
-            </ListItem>
-          );
-        })}
-      </List>
-      <Divider />
-
       <List>
         <h3>Updates</h3>
 
-        <Divider/>
-        {districtJSONData.map((item) => {
+        <Divider />
+        {districtJSONData.map(item => {
           return (
             <div>
-            <ListItem button >
-
-              <SimpleModal type={item.Type} priority={item.Priority} location={item.Location} description={item.Description} latitude={item.Latitude} longitude ={item.Longitude} />
-              &nbsp;
-              <ListItemText primary={item.Priority} secondary={item.Location} />
-
-            </ListItem>
-            <Divider/>
+              <ListItem button>
+                <SimpleModal
+                  type={item.Type}
+                  priority={item.Priority}
+                  location={item.Location}
+                  description={item.Description}
+                  latitude={item.Latitude}
+                  longitude={item.Longitude}
+                />
+                &nbsp;
+                <ListItemText
+                  primary={item.Priority}
+                  secondary={item.Location}
+                />
+              </ListItem>
+              <Divider />
             </div>
           );
         })}
